5734a50940a461b051ffd25d22ffa1adcf02e664
Arzhel <anthonyberleur@hotmail.fr>
2017-02-05 21:25:27 -0500

    123
    123

---
+++ Assets/Scripts/Collision2D.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision2D : MonoBehaviour
+{    
+    public LayerMask collisionMask;
+    
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+    PlayerControl player;
+
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+    Vector2 velocity;
+
+    void Start()
+    {
+        player = GetComponent<PlayerControl>();
+        collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    void Update()
+    {
+        UpdateRaycastOrigins();
+    }
+
+    public bool HorizontalCollisions(Vector3 velocity, float rayLength)
+    {
+        bool canMove = true;
+        float directionX = Mathf.Sign(velocity.x);
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                canMove = false;
+            }
+
+        }
+        return canMove;
+    }
+
+    public bool VerticalCollisions(Vector3 velocity, float rayLength)
+    {
+        bool canMove = true;
+        float directionY = Mathf.Sign(velocity.y);
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                canMove = false;
+            }
+        }
+        return canMove;
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    Vector3 pos;
+    Vector2 input;
+    Vector2 velocity;
+    float speed = 5f;
+    public LayerMask collisionMask;
+    public Vector2 gridSpace;
+    Collision2D collision;
+
+
+    void Start () {
+        collision = GetComponent<Collision2D>();
+        transform.position = Vector2.zero;
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("x"), Input.GetAxisRaw("y"));
+        if (pos == transform.position)
+        {
+            if (input.x != 0)
+            {
+                velocity = new Vector2(input.x * gridSpace.x, 0);
+                if (collision.HorizontalCollisions(velocity, gridSpace.x))
+                {
+                    pos += new Vector3(input.x * gridSpace.x, 0, 0);
+                }
+            }else if (input.y != 0)
+            {
+                velocity = new Vector2(0, input.y * gridSpace.y);
+                if (collision.VerticalCollisions(velocity,gridSpace.y))
+                {
+                    pos += new Vector3(0, input.y * gridSpace.y, 0);
+                }
+                
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
